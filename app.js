78b01935e3d554bce87f8a13b0812b406b05a536
Igor Souza <igorsouza.dev@gmail.com>
2020-05-24 11:21:59 -0300

    Add score and game over

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ function createMiniGrid() {
     i++;
   }
 }
+
 document.addEventListener('DOMContentLoaded', () => {
   let squares = createGridDivs();
   createMiniGrid();
@@ -32,13 +33,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const scoreDisplay = document.querySelector('#score');
   const startButton = document.querySelector('.start-button');
   let timerId;
+  let score = 0;
   const width = 10;
 
   const lTetromino = [
     [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
-    [width, width + 2, width * 2 + 1, width * 2 + 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
   ];
 
   const zTetromino = [
@@ -104,6 +106,45 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [
+        i,
+        i + 1,
+        i + 2,
+        i + 3,
+        i + 4,
+        i + 5,
+        i + 6,
+        i + 7,
+        i + 8,
+        i + 9,
+      ];
+
+      if (row.every((index) => squares[index].classList.contains('taken'))) {
+        score += 10;
+        scoreDisplay.innerHTML = score;
+        row.forEach((index) => {
+          squares[index].classList.remove('taken');
+          squares[index].classList.remove('tetromino');
+        });
+        const squaresRemoved = squares.splice(i, width);
+        squares = squaresRemoved.concat(squares);
+        squares.forEach((cell) => grid.appendChild(cell));
+      }
+    }
+  }
+
+  function gameOver() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains('taken')
+      )
+    ) {
+      scoreDisplay.innerHTML = 'end';
+      clearInterval(timerId);
+    }
+  }
   function draw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.add('tetromino');
@@ -130,6 +171,8 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4;
       draw();
       displayShape();
+      addScore();
+      gameOver();
     }
   }
   function moveDown() {
